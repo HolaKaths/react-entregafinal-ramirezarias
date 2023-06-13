ffb5b8bf55c292c43c6d89133cd74645c57fe5c0
Katheryn Ramirez <holakaths@gmail.com>
2023-06-13 23:02:54 +0200

    Ajustes css formularios

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { db } from '../../Services/config';
 import { collection, addDoc } from "firebase/firestore";
 import { useState } from 'react';
+import './Formulario.css';
 
 const Formulario = () => {
   const [nombre, setNombre] = useState("");
@@ -27,18 +28,24 @@ const Formulario = () => {
   }
 
   return (
-    <div className="form-container">
-      <form onSubmit={manejadorFormulario} className="form-style">
+    <div className="form-container" >
+      <form onSubmit={manejadorFormulario}>
 
         <h2>Formulario de contacto</h2>
-        <label htmlFor=""> Nombre </label>
-        <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        <div className="form-style">
+          <label htmlFor=""> Nombre </label>
+          <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        </div>
 
-        <label htmlFor=""> Apellido </label>
-        <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+        <div className="form-style">
+          <label htmlFor=""> Apellido </label>
+          <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+        </div>
 
-        <label htmlFor=""> Celular </label>
-        <input type="text" value={celular} onChange={(e) => setTelefono(e.target.value)} />
+        <div className="form-style">
+          <label htmlFor=""> Celular </label>
+          <input type="text" value={celular} onChange={(e) => setTelefono(e.target.value)} />
+        </div>
 
         <button type="submit">Enviar</button>
 
